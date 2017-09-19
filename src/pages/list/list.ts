0dfc93806d2c4b8c7b7a955294923e92a2039396
pxl9588 <pxl9588@g.rit.edu>
2017-09-19 16:30:25 -0400

    edited list

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -13,15 +13,14 @@ export class ListPage {
   items: Array<{title: string, note: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+    this.icons = ['american-football', 'baseball', 'football'];
 
     this.items = [];
-    for(let i = 1; i < 11; i++) {
+    for(let i = 0; i < this.icons.length; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+        title: 'Item ' + (i+1),
+        note: 'This #' + (i+1),
+        icon: this.icons[i]
       });
     }
   }
